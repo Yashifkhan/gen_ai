42d7edc3688a8c4b922753f5b86721fe41a0327d
yashif khan <yashifyashifkhan@gmail.com>
2026-01-13 23:40:06 +0530

    user ask question one by one in loop still user not esit or add bye

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ console.log("learn gen ai with node ");
 import groq from 'groq-sdk';
 import dotenv from 'dotenv'
 import { tavily } from '@tavily/core';
+import readline from 'readline/promises';
 dotenv.config()
 
 const groqApiKey = process.env.GROQ_API_KEY;
@@ -11,111 +12,101 @@ const tavilyClient = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 const userChat = async (propmt = "") => {
 
+    const rl=readline.createInterface({
+        input:process.stdin,
+        output:process.stdout
+    });
+
     const messages = [
         {
             role: "system",
             content: `You are a helpful assistant.
 Use tools ONLY when the user asks for real-time or current information.
-If tools are not required, answer normally `,
+If tools are not required, answer normally , current date or time ${new Date().toUTCString()}, you have access to the following tools:
+1. webSearch({query} :{query:string}) : searches the latest information and real time data on the internet
+`,
         },
-        {
-            role: 'user',
-            content: propmt,
-        }
+        
     ]
 
-    const complition = await groqClient.chat.completions.create({
-        model: "llama-3.3-70b-versatile",
-        messages: messages,
-        tools: [
-            {
-                type: 'function',
-                function: {
-                    name: 'webSearch',
-                    description: 'search the latest information and real time data on the internate',
-                    parameters: {
-                        type: 'object',
-                        properties: {
-                            query: {
-                                type: 'string',
-                                description: 'the search query to perform on the web'
-                            }
-                        },
-                        required: ['query']
+   while(true){
+    const question= await rl.question("You: ");
+
+    if (question.toLowerCase() === "exit" || question.toLowerCase() === "bye") {
+        break;
+    }
+
+    messages.push({ role: "user", content: question || propmt });
+
+
+
+     while (true) {
+        const complition = await groqClient.chat.completions.create({
+            model: "llama-3.3-70b-versatile",
+            // model: "openai/gpt-oss-120b",
+
+            messages: messages,
+            tools: [
+                {
+                    type: 'function',
+                    function: {
+                        name: 'webSearch',
+                        description: 'search the latest information and real time data on the internate',
+                        parameters: {
+                            type: 'object',
+                            properties: {
+                                query: {
+                                    type: 'string',
+                                    description: 'the search query to perform on the web'
+                                }
+                            },
+                            required: ['query']
+                        }
                     }
                 }
-            }
-        ],
-       tool_choice: {
-  type: "function",
-  function: { name: "webSearch" }
-}
+            ],
+            //        tool_choice: {
+            //   type: "function",
+            //   function: { name: "webSearch" }
+            // }
+            tool_choice: 'auto'
 
-    });
-
-
-    messages.push(complition.choices[0].message);
-    const toolCalls = complition.choices[0].message.tool_calls;
-
-    if (!toolCalls || toolCalls.length === 0) {
-        console.log("Final Answer: ", complition.choices[0].message.content);
-        return;
-    }
-
-    for (const tool of toolCalls) {
-        console.log("tool call ", tool);
-        const functionName = tool.function.name;
-        const functionParms = tool.function.arguments;
-
-
-
-        if (functionName === "webSearch") {
-            const toolResult = await webSearch(JSON.parse(functionParms));
-
-            // console.log("web search result ->>",result);
-
-
-
-            messages.push({
-                tool_call_id: tool.id,
-                role: 'tool',
-                name: functionName,
-                content: toolResult
-            })
-
+        });
+        messages.push(complition.choices[0].message);
+        const toolCalls = complition.choices[0].message.tool_calls;
+        if (!toolCalls || toolCalls.length === 0) {
+            console.log("Final Answer: ", complition.choices[0].message.content);
+            break;
         }
-
+    
+        for (const tool of toolCalls) {
+            console.log("tool call ", tool);
+            const functionName = tool.function.name;
+            const functionParms = tool.function.arguments;
+    
+    
+    
+            if (functionName === "webSearch") {
+                const toolResult = await webSearch(JSON.parse(functionParms));
+    
+                // console.log("web search result ->>",result);
+    
+    
+    
+                messages.push({
+                    tool_call_id: tool.id,
+                    role: 'tool',
+                    name: functionName,
+                    content: toolResult
+                })
+    
+            }
+    
+        }
     }
 
-    // console.log("complete mess go to llm", messages)
-    // result from websearch tool 
-    const complition2 = await groqClient.chat.completions.create({
-        model: "llama-3.3-70b-versatile",
-        messages: messages,
-        // tools: [
-        //     {
-        //         type: 'function',
-        //         function: {
-        //             name: 'webSearch',
-        //             description: 'search the latest information and real time data on the internate',
-        //             parameters: {
-        //                 type: 'object',
-        //                 properties: {
-        //                     query: {
-        //                         type: 'string',
-        //                         description: 'the search query to perform on the web'
-        //                     }
-        //                 },
-        //                 required: ['query']
-        //             }
-        //         }
-        //     }
-        // ],
-        // tool_choice: 'auto'
-
-    });
-
-    console.log("final result : ", JSON.stringify(complition2.choices[0].message, null, 2));
+   }
+   rl.close();
 
 
 }
@@ -123,11 +114,13 @@ If tools are not required, answer normally `,
 
 
 async function webSearch({ query }) {
-    const response = await tavilyClient.search(query, { limit: 3 });
+    console.log("web search tool call...");
+
+    const response = await tavilyClient.search(query);
     const finalresult = response?.results?.map((result) => result.content).join("\n\n");
     return finalresult;
 }
 
-userChat(`when was launched gpt 5 model?  `);
+userChat(`what is the current weather in hisar haryana web search?`);
 
 
