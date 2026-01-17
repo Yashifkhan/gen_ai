5ffa2fc15752e845d01038ab05784558c176b920
yashif khan <yashifyashifkhan@gmail.com>
2026-01-17 23:08:27 +0530

    improve few shot prompting or ui of chat

--- server/index.js
+++ server/index.js
@@ -4,43 +4,48 @@ import dotenv from 'dotenv'
 import { tavily } from '@tavily/core';
 import readline from 'readline/promises';
 dotenv.config()
- 
+
 const groqApiKey = process.env.GROQ_API_KEY;
 const groqClient = new groq({ apiKey: groqApiKey });
 const tavilyClient = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 
 const userChat = async (propmt = "") => {
-
-    const rl=readline.createInterface({
-        input:process.stdin,
-        output:process.stdout
-    });
-
     const messages = [
         {
             role: "system",
-            content: `You are a helpful assistant.
-Use tools ONLY when the user asks for real-time or current information.
-If tools are not required, answer normally , current date or time ${new Date().toUTCString()}, you have access to the following tools:
-1. webSearch({query} :{query:string}) : searches the latest information and real time data on the internet
-`,
+            content: `You are a Smart personal assistant.
+            if you know the answer to a question ,answer it directly in plain English.
+            if the answer is requires real-time or current information,local ,or up-to-date information or if you do not knoe the answer  use the available tools to find it .
+            you have access to the following tools:
+            searchweb - webSearch(query:string) : use this tool to search the internet for current information and real-time data or unknown information.
+            Decide when  to use your knowledge and when to use the tools. 
+
+            Examples : 
+            question : what is the current weather in hisar haryana web search?
+            answer : (use webSearch to find the current weather in hisar haryana)
+
+            question : who is the president of the united states?
+            answer : (use webSearch to find the current president of the united states)
+
+            question : who is the cm of delhi?
+            answer : The current Chief Minister of Delhi is rekha gupta.
+
+            question : Tell me the latest iTnews web search 
+            answer : (use webSearch to find the latest news)
+
+            current date or time ${new Date().toUTCString()}`,
         },
-        
+
     ]
 
-   while(true){
-    const question= await rl.question("You: ");
-
-    if (question.toLowerCase() === "exit" || question.toLowerCase() === "bye") {
-        break;
+    if (propmt.toLowerCase() === "exit" || propmt.toLowerCase() === "bye") {
+        return "chat ended";
     }
 
-    messages.push({ role: "user", content: question || propmt });
+    messages.push({ role: "user", content: propmt });
 
-
-
-     while (true) {
+    while (true) {
         const complition = await groqClient.chat.completions.create({
             model: "llama-3.3-70b-versatile",
             // model: "openai/gpt-oss-120b",
@@ -75,50 +80,46 @@ If tools are not required, answer normally , current date or time ${new Date().t
         messages.push(complition.choices[0].message);
         const toolCalls = complition.choices[0].message.tool_calls;
         if (!toolCalls || toolCalls.length === 0) {
-            console.log("Final Answer: ", complition.choices[0].message.content);
-            break;
+            // console.log("Final Answer: ", );
+            return complition.choices[0].message.content;
         }
-    
+
         for (const tool of toolCalls) {
             console.log("tool call ", tool);
             const functionName = tool.function.name;
             const functionParms = tool.function.arguments;
-    
-    
-    
+
             if (functionName === "webSearch") {
                 const toolResult = await webSearch(JSON.parse(functionParms));
-    
+
                 // console.log("web search result ->>",result);
-    
-    
-    
+
+
+
                 messages.push({
                     tool_call_id: tool.id,
                     role: 'tool',
                     name: functionName,
                     content: toolResult
                 })
-    
+
             }
-    
+
         }
     }
 
-   }
-   rl.close();
+
 
 
 }
 
 async function webSearch({ query }) {
     console.log("web search tool call...");
-
     const response = await tavilyClient.search(query);
     const finalresult = response?.results?.map((result) => result.content).join("\n\n");
     return finalresult;
 }
-
-userChat(`what is the current weather in hisar haryana web search?`);
+// userChat(`what is the current weather in hisar haryana web search?`);
+export default userChat;
 
 
--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ import  express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
+import userChat from './index.js';
+// import userChat from '.';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -14,8 +17,10 @@ app.get('/', (req, res) => {
 
 app.post('/api/v1/emo-chat', async (req, res) => {
     console.log("emo chat start");
-    
-    
+    const { message } = req.body;
+    console.log("Received message:", message);
+    const result = await userChat(message);
+    res.status(201).json({message: "Ans is get successfully",success: true,data: result });
 });
 
 app.listen(PORT, () => {
